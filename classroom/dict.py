b169a2bf0b64934a4a2ce25a0ae0876654e3b780
James Liu <liuchuanbo@gmail.com>
2019-01-19 20:25:55 -0500

    homework

---
+++ classroom/dict.py
@@ -0,0 +1,8 @@
+# define dict
+dict = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+print(dict)
+
+# Q1: add 'C':[7,8,9]
+# Q2: delete 'A'
+# Q3: 5 is in dict or not? True: key? False
+# Q4: seaching key='C'? True: list of 'C'
---
+++ classroom/list.py
@@ -0,0 +1,9 @@
+# define a list
+list = [1, 2, 3]
+
+# Q1: insert 4 at end
+# Q2: insert 0 at begin
+# Q3: delete 3 from list
+# Q4: delete 1 from list
+# Q5: searching 4 is in list or not? True/False
+# Q6: searching 3 is in list or not? True/False
